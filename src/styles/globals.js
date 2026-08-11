a10cec06b67ad893635f811dd56ab68bb40b6328
Finn <f1nndevgamer@gmail.com>
2026-08-11 11:42:19 +0200

    NO SCROLL

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -12,15 +12,25 @@ const GlobalStyles = createGlobalStyle`
   html {
     font-size: 62.5%;
     scroll-behavior: smooth;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
 
   }
   body {
     font-family: ${props => props.theme.fonts.main};
     font-size: 1.6rem;
+    overflow-x: hidden;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
     background: ${props => props.theme.colors.background1};
     color: ${props => props.theme.colors.primary1};
     cursor: default;
 
+  }
+  body::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+    display: none;
   }
   h1,h2,h3,h4,h5,h6,button {
     font-family: ${props => props.theme.fonts.title};
